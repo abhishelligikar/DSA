7bba216736a7aedffaa39d12ff70f796a7588a08
Abhishek Kulkarni <abhishelligikar@gmail.com>
2024-07-30 19:55:37 +0530

    Max of 3 integers

---
+++ Week 1 (22nd  - 28th July)/Assignments/Basics-1/6. Max of 3 elements.py
@@ -0,0 +1,16 @@
+a, b, c = map(int,(input("Enter 3 numbers separated by comma : ").split(",")))
+
+print(f"Value of a : {a} \n" f"Value of b : {b} \n" f"Value of c : {c}")
+
+def find_max_of_three_numbers(a,b,c):
+    if((a > b and a > c) or (a == b and a == c) or (a == b and c < a)):
+        return a
+    elif((b > a and b > c) or (b == c and a < b) or (c == a and b > c)):
+        return b
+    else:
+        return c
+    
+    
+print(f"Maximum of three numbers : {a}, {b} and {c} is : {find_max_of_three_numbers(a,b,c)}")
+
+print(f"Maximum of three numbers : {a}, {b} and {c} is : {max(a,b,c)}")
