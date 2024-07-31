25fb20cb09d8a48cf0052c3f8ad3a5cc0be4e83f
Abhishek Kulkarni <abhishelligikar@gmail.com>
2024-07-31 19:29:15 +0530

    Sum of Digits of number

---
+++ Week 1 (22nd  - 28th July)/Assignments/Basics-2/6. Sum of digits of a number.py
@@ -0,0 +1,69 @@
+originalNum = input("Enter the number : ")
+num = originalNum
+sumValue = 0
+
+for i in num:
+    sumValue += int(i)
+    
+print(sumValue)
+
+# Method -2 
+num = int(originalNum)
+sumValue = 0
+
+while num != 0:
+    digit = int(num%10)
+    sumValue += digit
+    num /= 10
+    
+print(sumValue)
+
+# Method - 3
+num = int(originalNum)
+sumValue = 0
+def findsumValue(num, sumValue):
+    if num == 0:
+        return sumValue
+    
+    digit = int(num%10)
+    sumValue += digit
+    return findsumValue(num/10, sumValue)
+
+print(findsumValue(num, sumValue))
+
+# Method - 4
+num = int(originalNum)
+sumValue = 0
+def findsumValue(num):
+    if num == 0:
+        return 0
+    
+    return int(num % 10) + findsumValue(num/10)
+
+print(findsumValue(num))
+
+# Method - 5
+num = int(originalNum)
+sumValue = 0
+
+for i in range(len(str(num))):
+    sumValue += ord(str(num)[i]) - 48    # ord methods helps with ASCII
+    
+print(sumValue)
+
+# Method - 6
+num = int(originalNum)
+sumValue = 0
+
+def getsumValue(num):
+    num_string = str(num)       # convert into string
+    
+    # fetch each individual char using strip method
+    # find comparable int and store it in map
+    # covert it into list
+    list_of_number = list(map(int, num_string.strip())) 
+    
+    return sum(list_of_number)
+
+print(getsumValue(num))
+
