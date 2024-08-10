8846a22027fd611d4a5515622572b140626b076c
Abhishek Kulkarni <abhishelligikar@gmail.com>
2024-08-10 09:44:31 +0530

    Update comment

--- Week 1 (22nd  - 28th July)/Assignments/Basics-1/6. Max of 3 elements.py
+++ Week 1 (22nd  - 28th July)/Assignments/Basics-1/6. Max of 3 elements.py
@@ -3,6 +3,7 @@ a, b, c = map(int,(input("Enter 3 numbers separated by comma : ").split(",")))
 print(f"Value of a : {a} \n" f"Value of b : {b} \n" f"Value of c : {c}")
 
 def find_max_of_three_numbers(a,b,c):
+    # Always use int_min or int_max for return value
     if((a > b and a > c) or (a == b and a == c) or (a == b and c < a)):
         return a
     elif((b > a and b > c) or (b == c and a < b) or (c == a and b > c)):
